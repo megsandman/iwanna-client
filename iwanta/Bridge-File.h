10a67a1a2363b668d3f0fa9202c77d389e031e65
Meg Sandman <margaret.sandman@gmail.com>
2015-06-23 18:56:58 -0700

    basic city table view setup

---
+++ iwanta/Bridge-File.h
@@ -0,0 +1,16 @@
+//
+//  Bridge-File.h
+//  iwanta
+//
+//  Created by Meg Sandman on 6/23/15.
+//  Copyright (c) 2015 Meg Sandman. All rights reserved.
+//
+
+#ifndef iwanta_Bridge_File_h
+#define iwanta_Bridge_File_h
+
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+#import "Flurry.h"
+
+#endif
